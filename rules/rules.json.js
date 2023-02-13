dd3e883103c0e91327a720d9b052bd0b5d40364b
Blink Chen <kingcyk@enjoymusic.ai>
2023-02-13 12:01:26 +0800

    update rules

--- rules/rules.json.js
+++ rules/rules.json.js
@@ -1,48 +1,48 @@
 {
   "gfwlist": {
     "name": "gfwlist.conf",
-    "date": "2022-11-14 21:58",
-    "md5": "2774233469e9a912b5d7fbb63a564fe0",
-    "count": "5591"
+    "date": "2023-02-13 11:49",
+    "md5": "c341b0c51ccc7a0e509618cb5fded6d8",
+    "count": "5554"
   },
   "chnroute": {
     "name": "chnroute.txt",
-    "date": "2022-11-14 21:58",
-    "md5": "0b81c3a1469b2b50898b7e4c5a044b43",
-    "count": "3277",
-    "count_ip": "273545574",
+    "date": "2023-02-13 11:49",
+    "md5": "90e44d60df5e37d441f9bf179f6e66aa",
+    "count": "3581",
+    "count_ip": "299911430",
     "source": "misakaio",
     "url": "https://github.com/misakaio/chnroutes2/blob/master/chnroutes.txt"
   },
   "chnroute2": {
     "name": "chnroute2.txt",
-    "date": "2022-07-16 23:02",
-    "md5": "f12a5a7fb6c9c37d52dbbea0666812ba",
-    "count": "6182",
-    "count_ip": "13240665434",
+    "date": "2023-02-13 11:49",
+    "md5": "129cb57658181d8948ace37f35bbca68",
+    "count": "6368",
+    "count_ip": "17534691524",
     "source": "ipip",
     "url": "https://github.com/firehol/blocklist-ipsets/blob/master/ipip_country/ipip_country_cn.netset"
   },
   "chnroute3": {
     "name": "chnroute3.txt",
-    "date": "2022-11-14 21:58",
-    "md5": "2b32a67a3a5c9155b8fcc4cb579f3f0d",
-    "count": "8619",
-    "count_ip": "343219626",
+    "date": "2023-02-13 11:49",
+    "md5": "26dcb26f7c10d13ab61d7ab7cf28b8f4",
+    "count": "8612",
+    "count_ip": "343179448",
     "source": "apnic",
     "url": "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
   },
   "cdn_china": {
     "name": "cdn.txt",
-    "date": "2022-11-14 21:58",
-    "md5": "69ad0a95b860b6a6345c055c96c1403a",
-    "count": "65325"
+    "date": "2023-02-13 11:49",
+    "md5": "a3f4b783bca40073c0c4e33e96fdb23f",
+    "count": "64717"
   },
   "apple_china": {
     "name": "apple_china.txt",
-    "date": "2022-10-31 23:23",
-    "md5": "dfaf9b2ad0ecb84fd0441ea2d01fe3dc",
-    "count": "134"
+    "date": "2023-02-13 11:49",
+    "md5": "62e7c6ae547a3ff664d050c35983f55c",
+    "count": "137"
   },
   "google_china": {
     "name": "google_china.txt",
